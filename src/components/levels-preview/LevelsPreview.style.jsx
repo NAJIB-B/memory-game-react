975df51a1da2bf21a0123bb03c619d0832d54088
NAJIB-B <najibajibasu@gmail.com>
2022-09-03 14:26:01 +0100

    fixed level preview color

--- src/components/levels-preview/LevelsPreview.style.jsx
+++ src/components/levels-preview/LevelsPreview.style.jsx
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const LevelPreviewDiv = styled.div`
   font-size: 1.5rem;
-  /* background-color: rgba(152, 68, 183, 1); */
-  background-color: aqua;
+  background-color: rgba(152, 68, 183, 1);
+
   height: 100vh;
   overflow: scroll;
   display: grid;
